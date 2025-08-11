b874c07951328e90a0b52fa504041cbbaae45feb
meowyoudoin <42545819+meowyoudoin@users.noreply.github.com>
2025-08-11 21:15:46 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'kopi-flow-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+  // Add icons or other static files here if you host them
+];
+
+self.addEventListener('install', (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (evt) => {
+  evt.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null)
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then(resp => resp || fetch(evt.request))
+  );
+});
